1f065e5347ab8b60d6d9f93ea98ed67b2b022e0c
Vasilkovskiy Artem <va.work.dev@gmail.com>
2021-12-23 16:33:48 +0800

    Initial Commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task08.task0801;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+/* 
+Set из растений
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("арбуз");
+        set.add("банан");
+        set.add("вишня");
+        set.add("груша");
+        set.add("дыня");
+        set.add("ежевика");
+        set.add("женьшень");
+        set.add("земляника");
+        set.add("ирис");
+        set.add("картофель");
+        //Collections.addAll(set,"банан", "вишня", "груша", "дыня", "ежевика", "женьшень", "земляника", "ирис", "картофель");
+
+        for (String text : set) {
+            System.out.println(text);
+        }
+
+    }
+}
