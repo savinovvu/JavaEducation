bee2e4db3887e3dcd041b0f828b1ddd8b5c71640
Vasilkovskiy Artem <va.work.dev@gmail.com>
2022-02-18 15:54:21 +0800

    task1523(AccessModifiers)

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    private Solution(int example) {
+    }
+
+    Solution(double example) {
+    }
+
+    protected Solution(String example) {
+    }
+
+    public Solution(Float example) {
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    SubSolution(double example) {
+        super(example);
+    }
+
+    protected SubSolution(String example) {
+        super(example);
+    }
+
+    public SubSolution(Float example) {
+        super(example);
+    }
+}
+
