c0d2d89743b974e8eaa659fc1647a18d9d7ce818
Vasilkovskiy Artem <va.work.dev@gmail.com>
2022-05-17 14:14:46 +0800

    task1903 (adapter,String.format phoneNumber  )

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task19.task1903;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Адаптация нескольких интерфейсов
+*/
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+
+        IncomeDataAdapter(IncomeData incomeData) {
+            this.data = incomeData;
+        }
+
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String number = String.format(("%010d"), data.getPhoneNumber());
+            String phoneNumber = "+" + String.format("%d(%2$s)%3$s-%4$s-%5$s", data.getCountryPhoneCode(),
+                    number.substring(0, 3), number.substring(3, 6),
+                    number.substring(6, 8), number.substring(8, 10));
+            return phoneNumber;
+        }
+
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+    }
+
+
+    public interface IncomeData {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example1: 501234567, For example2: 71112233
+    }
+
+    public interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example1: +38(050)123-45-67, For example2: +38(007)111-22-33
+    }
+}
